87bcd8c1075d417d3511fad7fbc2af07d31d4c3d
Cominch <dominic@luetjohann.com>
2013-06-10 17:13:22 +0200

    better IP detection

--- beaglebonechip.js
+++ beaglebonechip.js
@@ -1,13 +1,15 @@
 var util = require ('util');
 var exec = require ('child_process').exec;
 
+var os = require ('os');
+
 (function(exports) {
 
 	var ipaddress;
 	var serialnumber;
 
 	exports.getSerialNumber = function () {
-		
+
     	exec ("hexdump -e '8/1 \"%c\"' /sys/bus/i2c/devices/0-0050/eeprom -s 16 -n 12", function(error, stdout, stderr) {
 
     		serialnumber = stdout;
@@ -25,10 +27,20 @@ var exec = require ('child_process').exec;
 
     exports.getIPAddress = function () {
 
-    	exec ("hostname -i", function(error, stdout, stderr) {
+    	var interfaces = os.networkInterfaces();
+    	var addresses = [];
+    	for (k in interfaces) {
+    		for (k2 in interfaces[k]) {
+    			var address = interfaces[k][k2];
+    			if (address.family == "IPv4" && !address.internal) {
+    				ipaddress = address.address;
+    			}
+    		}
+    	}
 
-    		ipaddress = stdout;
-    	});
+    	if (!ipaddress) {
+    		ipaddress = "127.0.0.1";
+    	}
 
     }
   
